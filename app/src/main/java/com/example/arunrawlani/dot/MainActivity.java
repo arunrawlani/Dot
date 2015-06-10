c53b312880e5d0d87d4ef896e958418a6ee90c5a
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-06-09 20:28:16 -0400

    Added navigation drawer layout and pseudo content

--- app/src/main/java/com/example/arunrawlani/dot/MainActivity.java
+++ app/src/main/java/com/example/arunrawlani/dot/MainActivity.java
@@ -5,19 +5,38 @@ import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.List;
 
 
 public class MainActivity extends Activity{
+
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+    }
+
+    private void addDrawerItems(){
+        String[] osArray={"Android", "iOS", "Windows", "OS X"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
     }
 
     @Override
