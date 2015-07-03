8848aa8039c7b5fc6a9cf9499f39993fb5f0e025
AndiRayhan <andi.chibrandy@mail.mcgill.ca>
2015-07-03 17:57:58 -0400

    login works now

--- app/src/main/java/com/example/arunrawlani/dot/LoginActivity.java
+++ app/src/main/java/com/example/arunrawlani/dot/LoginActivity.java
@@ -3,19 +3,17 @@ package com.example.arunrawlani.dot;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 
 public class LoginActivity extends Activity {
@@ -65,7 +63,29 @@ public class LoginActivity extends Activity {
                     dialog.show();
                 }
                 else{
+                    ParseUser.logInInBackground(username, password, new LogInCallback() {
+                        @Override
+                        public void done(ParseUser parseUser, ParseException e) {
+                            if(parseUser != null){
+                                AlertDialog.Builder builder= new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage("Login success")
+                                        .setTitle("login success")
+                                        .setPositiveButton(android.R.string.ok, null);
 
+                                AlertDialog dialog= builder.create();
+                                dialog.show();
+                            }
+                            else{
+                                AlertDialog.Builder builder= new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.signup_error_title)
+                                        .setPositiveButton(android.R.string.ok, null);
+
+                                AlertDialog dialog= builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
 
                 }
 
